2ae9dcd50938234223fa3344695a1328bce308ef
Aya Jaradat <79999962+aya-jaradat@users.noreply.github.com>
2021-03-21 00:21:22 +0200

    Add files via upload

---
+++ src/phase2design/Item.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phase2design;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Item {
+    private List <Integer> w = new ArrayList<>();
+    
+}
